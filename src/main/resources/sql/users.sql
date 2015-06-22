4048a12a60080bdf7763802188ecf0de122ba607
梁鹏飞 <871999566@qq.com>
2015-06-23 00:08:07 +0800

    fix error

--- src/main/resources/sql/users.sql
+++ src/main/resources/sql/users.sql
@@ -1,6 +1,6 @@
 --
 -- 转存表中的数据 `users`
 --
-ues drp
+use drp
 INSERT INTO `users` (`id`, `created_by`, `created_on`, `status`, `updated_by`, `updated_on`, `address`, `code`, `email`, `gender`, `name`, `password`, `phone`, `type`, `username`) VALUES
 (1, NULL, '2015-03-18 11:25:34', 'ACTIVE', NULL, '2015-03-18 12:10:12', '', 'admin', '', 'MALE', 'admin', '17a73b924892695ff91e16b35ce100b7c64928e43d5a170a', '1324332345', 'MANAGER', NULL);
